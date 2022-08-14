bfa20a880a8e30ad0b635b300b04b46cb0c74741
Eric <efredin@gmail.com>
2022-08-14 17:12:07 -0600

    home page

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,9 +1,16 @@
 import React from 'React';
+import { Paper } from '@mui/material';
 import { Page } from './layout';
+import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
-    <Page>Welcome to Netheria</Page>
+    <Page>
+      <Paper sx={{ my: 2, p: 2 }}>
+        <p>Welcome to Netheria</p>
+        <p>Check out the <Link to="/octomize">Octomize dashboard</Link></p>
+      </Paper>
+    </Page>
   );
 };
 
